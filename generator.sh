8702c76fe02e6bbb20f6858800593acbc47eef93
Matthew Cupples <mcupples@cloudspace.com>
2014-10-02 12:01:24 -0400

    added the generator

---
+++ generator.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+# Read Only Team Generator
+
+# here's where you put your oauth token
+token=""
+
+
+# this is the org name
+org=""
+
+
+# this is the id for team
+team_id=""
+
+
+# get all members from the organization
+# https://developer.github.com/v3/orgs/members/#members-list
+IFS=$'\r\n' GLOBIGNORE='*' :; members_array=($(curl -H "Authorization: token $token" -i https://api.github.com/orgs/$org/members|grep "\"login\": "|cut -d ":" -f 2|sed 's/"//g'|sed 's/,//g'|sed 's/ //g'))
+
+
+# add every member to the team
+# https://developer.github.com/v3/orgs/teams/#add-team-member
+for i in "${members_array[@]}"
+do
+  :
+  curl --request PUT -H "Content-Length: 0" -H "Authorization: token $token" -i "https://api.github.com/teams/$team_id/members/$i"
+done
+
+# get a list of every private repository in the organization
+# github limits us to 100 per page so fix this code, it's embarrassing
+# https://developer.github.com/v3/repos/#list-organization-repositories
+IFS=$'\r\n' GLOBIGNORE='*' :; repos1_array=($(curl -H "Authorization: token $token" -i "https://api.github.com/orgs/$org/repos?page=1&per_page=100"|grep "\"name\": "|cut -d ":" -f 2|sed 's/"//g'|sed 's/,//g'|sed 's/ //g'))
+IFS=$'\r\n' GLOBIGNORE='*' :; repos2_array=($(curl -H "Authorization: token $token" -i "https://api.github.com/orgs/$org/repos?page=2&per_page=100"|grep "\"name\": "|cut -d ":" -f 2|sed 's/"//g'|sed 's/,//g'|sed 's/ //g'))
+
+
+# add every private repository to the team for the organization
+# https://developer.github.com/v3/orgs/teams/#add-team-repo
+#PUT /teams/$team_id/repos/$org/$repo
+for i in "${repos1_array[@]}"
+do
+  :
+  curl --request PUT -H "Content-Length: 0" -H "Authorization: token $token" -i "https://api.github.com/teams/$team_id/repos/$org/$i"
+done
+
+for i in "${repos2_array[@]}"
+do
+  :
+  curl --request PUT -H "Content-Length: 0" -H "Authorization: token $token" -i "https://api.github.com/teams/$team_id/repos/$org/$i"
+done
